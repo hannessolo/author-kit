e74a0635154bee944b00b1c67c290d19e4014045
Hannes Hertach <hanneshertach@gmail.com>
2026-01-08 17:12:03 +0100

    fix branch name

--- tools/quick-edit/quick-edit.js
+++ tools/quick-edit/quick-edit.js
@@ -9,7 +9,7 @@ export default function init(payload) {
   const { search } = window.location;
   const ref = new URLSearchParams(search).get('quick-edit');
   let origin;
-  if (ref === 'on' || !ref) origin = 'https://da-fusion--da-nx--adobe.aem.live';
+  if (ref === 'on' || !ref) origin = 'https://main--da-nx--adobe.aem.live';
   if (ref === 'local') origin = 'http://localhost:6456';
   if (!origin) origin = `https://${ref}--da-nx--adobe.aem.live`;
   loadMoudle(origin, payload);
